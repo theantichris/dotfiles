ec2132a216576d971748f334891b101cfbfe8c7f
Christopher Lamm <christopherlamm81@gmail.com>
2022-07-11 13:56:04 -0400

    Added powerlevel10k

--- install.sh
+++ install.sh
@@ -13,10 +13,10 @@ echo "Installing packages..."
 sudo apt install -y bat
 
 # Clone zsh plugins
-# TODO: clone powerlevel10k
 echo "Cloning zsh plugins..."
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 # Copy dotfiles
 echo "Copying dotfiles..."

a9521ce4502aa67875cf6e6865ef099415bc09c5
Christopher Lamm <christopherlamm81@icloud.com>
2025-10-25 09:07:52 -0400

    Add .config/fish/config.fish

---
+++ dot_config/fish/config.fish
@@ -0,0 +1,10 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
+
+# uv
+fish_add_path "/home/christopher/.local/bin"

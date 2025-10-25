e86ca5f79a774d6d87f3d6a50777e6b155751ae3
Christopher Lamm <christopherlamm81@icloud.com>
2025-10-25 19:12:13 -0400

    remove Windows and Ubuntu stuff

--- windows/export-winget.ps1
+++
@@ -1,7 +0,0 @@
-param(
-  [string]$Output = "$PSScriptRoot\winget-packages.json"
-)
-
-winget source update | Out-Null
-winget export --source winget --output "$Output" --accept-source-agreements
-Write-Host "Exported to $Output"

0d9135bdbe6a6f27227e28abb06a642c75f0b11d
Christopher Lamm <christopherlamm81@icloud.com>
2025-11-09 11:29:23 -0500

    Update .config/fish/config.fish Update .config/fish/functions/history.fish

---
+++ dot_config/fish/functions/empty_history.fish
@@ -0,0 +1,34 @@
+function __history_previous_command
+    switch (commandline -t)
+        case "!"
+            commandline -t $history[1]
+            commandline -f repaint
+        case "*"
+            commandline -i !
+    end
+end
+
+function __history_previous_command_arguments
+    switch (commandline -t)
+        case "!"
+            commandline -t ""
+            commandline -f history-token-search-backward
+        case "*"
+            commandline -i '$'
+    end
+end
+
+if [ "$fish_key_bindings" = fish_vi_key_bindings ]
+
+    bind -Minsert ! __history_previous_command
+    bind -Minsert '$' __history_previous_command_arguments
+else
+    bind ! __history_previous_command
+    bind '$' __history_previous_command_arguments
+end
+
+## Fish command history
+
+function history
+    builtin history --show-time='%F %T '
+end

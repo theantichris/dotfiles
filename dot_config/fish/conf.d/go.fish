9ad435bbe3f5610f1398a43c31b6c49e687a493c
Christopher Lamm <christopherlamm81@icloud.com>
2025-11-16 17:35:13 -0500

    Update .config/fish/conf.d/go.fish

--- dot_config/fish/conf.d/go.fish
+++ dot_config/fish/conf.d/go.fish
@@ -19,7 +19,7 @@ alias got='go test'
 alias gota='go test ./...'
 alias gotc='go test ./... -cover --coverprofile coverage.out; and gocovsh'
 alias gote="go test -tag=e2e"
-alias gotw='fd -e go -E vendor -E gen -E dist | entr -c gotestsum -- -cover ./...'
+alias gotw='fd -e go -E vendor -E gen -E dist | entr -c gotestsum -- -skip \'e2e_test.*\' -cover ./...'
 alias goto='go tool'
 alias gotoc='go tool compile'
 alias gotod='go tool dist'

95fc513b26548a7b4c336247ef000a8f8a92b8d0
Christopher Lamm <christopherlamm81@gmail.com>
2026-01-10 14:34:07 -0500

    Update .config/fish/environment.fish

--- dot_config/fish/environment.fish
+++ dot_config/fish/environment.fish
@@ -2,7 +2,7 @@
 set -U __done_min_cmd_duration 10000
 set -U __done_notification_urgency_level_low
 
-set -Ux BROWSER google-chrome-stable
+set -Ux BROWSER firefox
 
 set -Ux SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/ssh-agent.socket"
 ssh-add ~/.ssh/github_ed25519 &>/dev/null

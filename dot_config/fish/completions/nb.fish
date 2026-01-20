5d0453473e7b9cb856cba45a202ebcbf8c2c87ea
Christopher Lamm <christopherlamm81@gmail.com>
2026-01-20 11:22:10 -0500

    Add .config/fish/completions/nb.fish

---
+++ dot_config/fish/completions/nb.fish
@@ -0,0 +1,14 @@
+(B[m[38;5;51mUsage(B[m:
+  nb completions (B[m[90m((B[mcheck (B[m[90m|(B[m install (B[m[90m[(B[m-d (B[m[90m|(B[m --download(B[m[90m](B[m (B[m[90m|(B[m uninstall(B[m[90m)(B[m
+
+(B[m[38;5;51mOptions(B[m:
+  -d, --download  Download the completion scripts and install.
+
+(B[m[38;5;51mDescription(B[m:
+  Manage completion scripts.
+
+(B[m[38;5;51mRead More(B[m:
+  https://github.com/xwmx/nb/blob/master/etc/README.md
+
+(B[m[38;5;51mSee Also(B[m:
+  nb help env

c1868bec34093416ed37ce39ac42b2aafa1c886d
Christopher Lamm <christopherlamm81@gmail.com>
2026-01-24 19:26:16 -0500

    added 2nd bottom pane for ide mode

--- dot_config/fish/functions/ide.fish
+++ dot_config/fish/functions/ide.fish
@@ -3,8 +3,10 @@ function ide --description "Open IDE-style pane layout"
     wezterm cli split-pane --right --percent 30
     # Move back to left pane
     wezterm cli activate-pane-direction left
-    # Split bottom pane (15% height)
-    wezterm cli split-pane --bottom --percent 15
+    # Split bottom pane (15% height) and capture its pane ID
+    set bottom_pane_id (wezterm cli split-pane --bottom --percent 15)
+    # Split bottom pane horizontally into two using its ID
+    wezterm cli split-pane --right --percent 50 --pane-id $bottom_pane_id
     # Focus top-left (main editing area)
     wezterm cli activate-pane-direction up
 end

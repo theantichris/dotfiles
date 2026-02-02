162add9c4e67cff10ebe1dcaf4c3dfbe5ca2247e
Christopher Lamm <christopherlamm81@gmail.com>
2026-02-02 11:12:46 -0500

    update ide function to open ghost how I want

--- dot_config/fish/functions/ide.fish
+++ dot_config/fish/functions/ide.fish
@@ -1,12 +1,42 @@
-function ide --description "Open IDE-style pane layout"
-    # Split right pane (30% width)
-    wezterm cli split-pane --right --percent 30
+function ide --description "Open IDE-style pane layout" --argument project
+    # Set working directory based on project argument
+    if test -n "$project"
+        set project_dir ~/Code/$project
+        if not test -d $project_dir
+            echo "Project directory not found: $project_dir"
+            return 1
+        end
+        set cwd_flag --cwd $project_dir
+    else
+        set cwd_flag
+    end
+
+    # Split right pane (30% width) - runs claude if project specified
+    if test -n "$project"
+        wezterm cli split-pane --right --percent 30 $cwd_flag -- claude
+    else
+        wezterm cli split-pane --right --percent 30
+    end
+
     # Move back to left pane
     wezterm cli activate-pane-direction left
+
     # Split bottom pane (15% height) and capture its pane ID
-    set bottom_pane_id (wezterm cli split-pane --bottom --percent 15)
-    # Split bottom pane horizontally into two using its ID
-    wezterm cli split-pane --right --percent 50 --pane-id $bottom_pane_id
+    set bottom_pane_id (wezterm cli split-pane --bottom --percent 15 $cwd_flag)
+
+    # Split bottom pane horizontally - right side runs gotw if project specified
+    if test -n "$project"
+        wezterm cli split-pane --right --percent 50 --pane-id $bottom_pane_id $cwd_flag -- fish -c gotw
+    else
+        wezterm cli split-pane --right --percent 50 --pane-id $bottom_pane_id
+    end
+
     # Focus top-left (main editing area)
     wezterm cli activate-pane-direction up
+
+    # Run helix in main pane if project specified
+    if test -n "$project"
+        cd $project_dir
+        hx .
+    end
 end

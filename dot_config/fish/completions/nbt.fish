8079dbf0af54633bd9dee984fa68cafe26d2f723
Christopher Lamm <christopherlamm81@gmail.com>
2026-02-22 21:12:48 -0500

    Remove .config/fish/completions/nbt.fish

--- dot_config/fish/completions/nbt.fish
+++
@@ -1,7 +0,0 @@
-function __nbt_templates
-    for f in ~/.nb/home/templates/*.md
-        basename $f .md
-    end
-end
-
-complete -c nbt -f -n "__fish_use_subcommand" -a "(__nbt_templates)" -d "Template"

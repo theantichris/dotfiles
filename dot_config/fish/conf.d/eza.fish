468dfa600197c2625597ac96ed69427ed41c36eb
Christopher Lamm <christopherlamm81@gmail.com>
2026-07-09 10:18:37 -0400

    fix eza aliases

--- dot_config/fish/conf.d/eza.fish
+++ dot_config/fish/conf.d/eza.fish
@@ -1,6 +1,6 @@
 source ~/.config/eza/theme.env
 
-alias ls='eza --group-directories-first --color=auto --icons'
-alias ll='eza -l --group-directories-first --git --icons'
-alias la='eza -la --group-directories-first --git --icons'
-alias lt='eza -l --tree --level=2 --group-directories-first --icons'
+alias ls='eza --group-directories-first --color=auto --icons=auto'
+alias ll='eza -l --group-directories-first --git --icons=auto'
+alias la='eza -la --group-directories-first --git --icons=auto'
+alias lt='eza -l --tree --level=2 --group-directories-first --icons=auto'

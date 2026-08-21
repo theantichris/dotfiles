a08c1fdc61aa75cde091f1718b9ad64097f332ef
Christopher Lamm <christopherlamm81@gmail.com>
2026-08-21 10:06:22 -0400

    Update .config/fish/environment.fish Update .config/hypr/config/variables.lua

--- dot_config/hypr/config/variables.lua
+++ dot_config/hypr/config/variables.lua
@@ -2,7 +2,7 @@
 
 TERMINAL     = "kitty"
 FILE_MANAGER = "dolphin"
-BROWSER      = "google-chrome-stable"
+BROWSER      = "zen-browser"
 EDITOR       = "helix"
 --EDITOR       = "gnome-text-editor --new-window"
 CALCULATOR   = "gnome-calculator"

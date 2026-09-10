5a0f768cf9c56db41a0ef245a8cadbb128937b4b
Christopher Lamm <christopherlamm81@gmail.com>
2026-09-10 11:53:49 +0000

    fish: add Parrot VM desktop launcher

---
+++ dot_config/fish/functions/parrot.fish
@@ -0,0 +1,17 @@
+function parrot --description 'Start Parrot if needed and open its desktop'
+    set -l uri qemu:///system
+    set -l state (env LC_ALL=C virsh --connect $uri domstate htb-parrot)
+    or return 1
+
+    if test "$state" = 'shut off'
+        command virsh --connect $uri start htb-parrot
+        or return 1
+    else if test "$state" != running
+        printf 'Parrot is %s; not opening the viewer.\n' "$state" >&2
+        return 1
+    end
+
+    # Attach through libvirt and leave the terminal free for other commands.
+    command virt-viewer --connect $uri --attach htb-parrot &
+    disown $last_pid
+end

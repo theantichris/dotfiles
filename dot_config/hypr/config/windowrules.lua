472316da987ee78383c593773a68bac041c11b7a
Christopher Lamm <christopherlamm81@gmail.com>
2026-09-12 09:08:27 -0400

    focus scratchpad terminal when visible

--- dot_config/hypr/config/windowrules.lua
+++ dot_config/hypr/config/windowrules.lua
@@ -54,7 +54,7 @@ hl.window_rule({ match = { class = "^(org\\.kde\\.keditfiletype)$" }, float = tr
 hl.window_rule({ match = { class = "^(org\\.kde\\.ark)$" }, size = { "max(monitor_w, monitor_h)*0.40", "min(monitor_w, monitor_h)*0.40" } })
 hl.window_rule({ match = { class = "^(.*satty.*)$", title = "^(Satty)$" }, min_size = { "max(monitor_w, monitor_h)*0.35", "min(monitor_w, monitor_h)*0.35" }, float = true })
 hl.window_rule({ match = { class = "^(dev\\.)?(noctalia\\.Noctalia(\\.Settings)?)$" }, float = true, size = { "monitor_w*0.70", "monitor_h*0.70" } })
-hl.window_rule({ match = { class = "^(kitty-scratchpad)$" }, workspace = "special:terminal" })
+hl.window_rule({ match = { class = "^(kitty-scratchpad)$" }, workspace = "special:terminal", stay_focused = true })
 hl.window_rule({
     match = {
         class = "^(org\\.kde\\.dolphin)$",

655c40a38058a5addbf9332b7383d39d80035ffb
Christopher Lamm <christopherlamm81@gmail.com>
2026-09-12 08:46:42 -0400

    Make window animations snappier

--- dot_config/hypr/config/animations.lua
+++ dot_config/hypr/config/animations.lua
@@ -14,7 +14,7 @@ hl.curve("rubber",         { type = "spring", mass = 1, stiffness = 200,  dampen
 
 -- Animations
 hl.animation({ leaf = "global",              enabled = true, speed = 3, bezier = "quick"                 })
-hl.animation({ leaf = "windows",             enabled = true, speed = 3, spring = "easy",  style = "slide" })
+hl.animation({ leaf = "windows",             enabled = true, speed = 3, bezier = "quick", style = "slide" })
 hl.animation({ leaf = "workspaces",          enabled = true, speed = 5, bezier = "quick", style = "slide" })
 hl.animation({ leaf = "specialWorkspaceIn",  enabled = true, speed = 2, bezier = "quick", style = "slide top"})
 hl.animation({ leaf = "specialWorkspaceOut", enabled = true, speed = 2, bezier = "quick", style = "slide bottom"})
